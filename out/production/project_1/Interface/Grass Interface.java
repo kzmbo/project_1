c2b58f21a1370a280ee5cc15e44048ed7fe0a0b3
NoMamesWey1 <92000349+NoMamesWey1@users.noreply.github.com>
2021-10-15 21:03:57 -0700

    Add files via upload

---
+++ out/production/project_1/Interface/Grass Interface.java
@@ -0,0 +1,8 @@
+public interface Grass{
+  String specialMenu = "1. Vine Whip\n2. Razor Leaf\n3. Solar Beam";
+  int numSpecialMenuItems = 3;
+  String vineWhip(Pokemon p);
+  String razorLeaf(Pokemon p);
+  String solarBeam(Pokemon p);
+
+}
